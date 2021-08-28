1995bbd28b06bbe2c98bd3562c3b96c1cace1e11
sim-eunzi <kokopink12@soongsil.ac.kr>
2021-08-28 10:01:52 +0900

    06-4 색 데이터 삭제

--- 06/06-3/src/App.js
+++ 06/06-3/src/App.js
@@ -3,10 +3,13 @@ import colorData from './data/color-data.json'
 import ColorList from './components/ColorList'
 
 function App() {
-  const [colors] = useState(colorData)
+  const [colors, setColors] = useState(colorData)
 
   return (
-    <ColorList colors={colors.colors} />
+    <ColorList colors={colors.colors} onRemoveColor={id => {
+      const newColors = colors.colors.filter(color => color.id !== id)
+      setColors({ colors: newColors })
+    }} />
   );
 }
 
--- 06/06-3/src/components/ColorList.js
+++ 06/06-3/src/components/ColorList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Color from './Color'
 
-export default function ColorList({ colors }) {
+export default function ColorList({ colors = [], onRemoveColor = f => f }) {
+  console.log(colors)
   if(!colors.length) return <div>표시할 색이 없습니다</div>
 
   return (
     <div>
-      { colors.map(color => <Color key={color.id} {...color} />)}
+      { colors.map(color => <Color key={color.id} {...color} onRemove={id => onRemoveColor(id)} />)}
     </div>
   )
 }
+
